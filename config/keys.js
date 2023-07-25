2f20305a5f780cf10bf59c147eb7ab73f4b2c64d
Kamlesh Bhojane <126494618+kamlesh2k@users.noreply.github.com>
2023-07-25 17:56:42 +0530

    updated mongoose url

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    MongoURI: 'mongodb://127.0.0.1:27017/habit_tracker_db'    
+    // MongoURI: 'mongodb://127.0.0.1:27017/habit_tracker_db'    
+    MongoURI:'mongodb+srv://kamleshbhojane18:wPZR7PtgedWi5Fq8@cluster0.hllk1y1.mongodb.net/habbit_tracker_db'
 }
